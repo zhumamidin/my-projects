8e09116c53f7466c7c50940f6f9cb11c74cb6399
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2022-01-11 23:47:33 +0600

    Added new styles and media styles

--- The-loop/js/main.js
+++ The-loop/js/main.js
@@ -5,10 +5,18 @@ $(document).ready(function () {
   });
 });
 
-const menuItem = document.querySelector(".menu__item");
+const menuToggler = document.querySelector(".header__menu-toggler");
 
-const subMenu = document.querySelector(".header__submenu");
+const menu = document.querySelector(".header__nav");
 
-menuItem.addEventListener("click", function () {
-  subMenu.classList.toggler("js-submenu-show");
+menuToggler.addEventListener("click", function () {
+  menu.classList.toggle("js-menu-show");
+});
+
+const linkToggler = document.querySelector(".header__menu-toggler");
+
+const links = document.querySelector(".header__links");
+
+linkToggler.addEventListener("click", function () {
+  links.classList.toggle("js-link-show");
 });
